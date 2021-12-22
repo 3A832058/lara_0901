ad5e4d2de87eb59fc78619ef4e079da281cd2f25
Xin-Ting Ke <greenpepper0606@gmail.com>
2021-12-22 09:48:17 +0800

    單元二 練習2:設定Form Request(更正)

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -20,14 +20,4 @@ class PostsController extends Controller
         $data = ['post' => $post];
         return view('posts.show', $data);
     }
-
-    public function store(PostRequest $request)
-    {
-        //
-    }
-
-    public function update(PostRequest $request)
-    {
-        //
-    }
 }
